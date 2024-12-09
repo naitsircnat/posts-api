aa83e1828a44d605a5a79c3642191fd2239bd56f
naitsircnat <cristiangerardchen@gmail.com>
2024-12-09 03:53:03 +0000

    Added route to view all posts

--- index.js
+++ index.js
@@ -19,12 +19,41 @@ const mongoClient = require("mongodb").MongoClient;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const mongoUri = process.env.MONGO_URI;
+const dbName = "sample_training";
 
-const main = () => {
-  app.get("/", (req, res) => {
-    res.json({ Message: "Hello world" });
+const connect = async (db, uri) => {
+  let client = await mongoClient.connect(uri);
+  let _db = client.db(db);
+  return _db;
+};
+
+const main = async () => {
+  let db = await connect(dbName, mongoUri);
+
+  // Show all posts
+  app.get("/posts", async (req, res) => {
+    try {
+      let results = await db
+        .collection("posts")
+        .find()
+        .project({
+          _id: 0,
+          permalink: 0,
+          comments: 0,
+        })
+        .limit(20)
+        .toArray();
+
+      res.json({ results });
+    } catch (error) {
+      console.error("Error:", error);
+      res
+        .status(500)
+        .json({ "Error retrieving posts": "Internal server error" });
+    }
   });
 };
 
